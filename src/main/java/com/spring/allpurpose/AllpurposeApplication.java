571564345bb0577fdcec2da880fb47c3871b0bdb
yangdonghao <littlesheepdy@163.com>
2023-11-20 13:52:22 +0800

    第一次提交

---
+++ src/main/java/com/spring/allpurpose/AllpurposeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.allpurpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllpurposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllpurposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/allpurpose/AllpurposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.allpurpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllpurposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
